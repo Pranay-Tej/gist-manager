3b97bf71700b72462348a7b1bcb9a1cef3ee0e73
pranay_teja <s.pranayteja@gmail.com>
2020-04-21 18:10:58 +0530

    add checkbox styles; change primary color; add width height to tagEditor buttons

--- react-ui/user-ui/src/components/library/code/TagEditor/TagCheckbox/TagCheckbox.js
+++ react-ui/user-ui/src/components/library/code/TagEditor/TagCheckbox/TagCheckbox.js
@@ -1,22 +1,28 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "./tagCheckbox.module.css";
 
 function TagCheckbox(props) {
     const { tag, checked, addToTagSet, removeFromTagSet } = props;
 
-
     const updateTagSet = (event) => {
-        if(event.target.checked === true){
-            addToTagSet(tag.id)
-        }else{
-            removeFromTagSet(tag.id)
+        if (event.target.checked === true) {
+            addToTagSet(tag.id);
+        } else {
+            removeFromTagSet(tag.id);
         }
-    }
+    };
 
     return (
         <div className={styles["tag-checkbox"]}>
-            <input type="checkbox" checked={checked} onChange={updateTagSet}/>
+            <label className={styles["checkbox-label"]}>
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={updateTagSet}
+                />
+                <span className={`${styles["checkbox-custom"]} ${styles["rectangular"]}`}></span>
+            </label>
             <div className={styles["tag-name"]}>{tag.name}</div>
         </div>
     );
